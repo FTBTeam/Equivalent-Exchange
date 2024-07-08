ecbb5e0285aef73e00939613fe677d5d8b2cad8e
Gigabit101 <thegigabit101@gmail.com>
2024-07-08 14:20:12 +0100

    Remove entity

--- common/src/main/java/net/creeperhost/equivalentexchange/blocks/BlockNovaCatalyst.java
+++ common/src/main/java/net/creeperhost/equivalentexchange/blocks/BlockNovaCatalyst.java
@@ -1,6 +1,5 @@
 package net.creeperhost.equivalentexchange.blocks;
 
-import net.creeperhost.equivalentexchange.entities.EntityNovaCatalyst;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
